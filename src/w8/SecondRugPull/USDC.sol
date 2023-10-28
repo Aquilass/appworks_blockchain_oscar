96e359381ee860cea24c98c9b41268de02cc9e83
richardu15 <richard.u15@gmail.com>
2023-10-28 19:42:42 +0800

    w8 hw8

--- src/w8/SecondRugPull/USDC.sol
+++ src/w8/SecondRugPull/USDC.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract usdcV2 is ERC20 {
+contract usdcV2 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    string public currency;
+    address public masterMinter;
+    bool internal initialized;
+
+    mapping(address => uint256) internal balances;
+    mapping(address => mapping(address => uint256)) internal allowed;
+    uint256 internal totalSupply_ = 0;
+    mapping(address => bool) internal minters;
+    mapping(address => uint256) internal minterAllowed;
+
+    event Mint(address indexed minter, address indexed to, uint256 amount);
+    event Burn(address indexed burner, uint256 amount);
+    event MinterConfigured(address indexed minter, uint256 minterAllowedAmount);
+    event MinterRemoved(address indexed oldMinter);
+    event MasterMinterChanged(address indexed newMasterMinter);
+
     function VERSION() public pure returns (string memory) {
         return "usdcV2";
     }
