d7edd9518b8369d63dbba31c472da4f169d63fc7
oscar <richard.u15@gmail.com>
2023-11-19 16:41:22 +0800

    forge install

--- hw10/SimpleSwap/contracts/SimpleSwap.sol
+++ hw10/SimpleSwap/contracts/SimpleSwap.sol
@@ -5,5 +5,17 @@ import { ISimpleSwap } from "./interface/ISimpleSwap.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
-    // Implement core logic here
+    address tokenA;
+    address tokenB;
+    constructor(address _tokenA, address _tokenB) ERC20("SimpleSwap", "SSWAP") {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+    // 檢查 tokenA, tokenB 是否為可使用的 token
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+    }
 }
